4b751438a68d22e0445f36db8950afa3e6f99bf0
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-29 09:23:00 -0700

    add get method for urls_new and implement a fuction to generate a random 6 char string aplhanumerically

--- express_server.js
+++ express_server.js
@@ -2,7 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// setting the ejs engine for our express app
 app.set("view engine", "ejs");
+
+// parses body for POST, to be legible to humans
+app.use(express.urlencoded({ extended: true }));
+
+// returns a string of 6 random alphanumeric chars
+function generateRandomString() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randString = "";
+
+  for (let i = 0; i < 6; i++) {
+    let randIndex = Math.floor(Math.random() * chars.length);
+    randString += chars.charAt(randIndex);
+  }
+
+  return randString;
+
+
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +35,15 @@ app.get("/urls", (req, res) => {
   // renders the url_index template and passes the var above as the info shown to user
   res.render(`urls_index`, templateVars);
  });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
  // user can search for specific tinyURL code to see its true URL and go to site, if known
 app.get("/urls/:id", (req, res) => {
