76556b07422808d03b29226df86047b01f73c1b5
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-29 18:40:07 -0700

    add ability with cookieParser to pass username to html conditional

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser'); 
 
 // setting the ejs engine for our express app
 app.set("view engine", "ejs");
 
 // parses body for POST, to be legible to humans
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // returns a string of 6 random alphanumeric chars
 function generateRandomString() {
@@ -32,15 +34,23 @@ let urlDatabase = {
 // shows the entire db of urls in /urls
 app.get("/urls", (req, res) => {
 
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
+
   // renders the url_index template and passes the var above as the info shown to user
   res.render(`urls_index`, templateVars);
  });
 
  // shows page to create new url
 app.get("/urls/new", (req, res) => {
+  
+  const templateVars = { 
+    username: req.cookies["username"],
+  };
 
-  res.render("urls_new");
+  res.render("urls_new", templateVars);
 });
 
 // allows user to create new tinyurl and have it saved in global bd
@@ -86,7 +96,7 @@ app.post("/urls/:id/update", (req, res) => {
 
 // redirects user to longURL site
 app.get("/u/:id", (req, res) => {
-
+  
   const id = req.params.id;
   const longURL = urlDatabase[id];
 
@@ -98,7 +108,11 @@ app.get("/urls/:id", (req, res) => {
   // takes the searched parameter in url and shows user the url they were looking for
   const urlID = req.params.id;
   // needs square bracket notation in order to show longURL
-  const templateVars = { id: urlID, longURL: urlDatabase[urlID]};
+  const templateVars = { 
+    id: urlID, 
+    longURL: urlDatabase[urlID],
+    username: req.cookies["username"],
+  };
   res.render(`urls_show`, templateVars);
  });
  
