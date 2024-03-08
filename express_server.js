2f0629170660ff5004c4c4f79060d0f6b1fc00ff
nicoayamen <nicolas.ayala4@gmail.com>
2024-03-07 19:20:15 -0700

    redo all to accomodate cookieSession. all gets templateVars changed.

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const { getUserByEmail, generateRandomString, isLoggedIn, isNotLoggedIn, postURLProtect, checkShortURL, users, urlDatabase } = require('./functions/helperFunctions');
 
 // setting the ejs engine for our express app
@@ -9,13 +9,20 @@ app.set("view engine", "ejs");
 
 // parses body for POST, to be legible to humans
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser()); // parses cookie to show
+// app.use(cookieParser()); // parses cookie to show
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
 // shows the entire db of urls in /urls
 app.get("/urls", (req, res) => {
 
+  const userId = req.session.user_id;
+  const user = users[userId];
+
   const templateVars = {
-    user: req.cookies["user_id"], // passes user_id to front end conditional
+    user: user, // passes user_id to front end conditional
     urls: urlDatabase,
   };
 
@@ -26,8 +33,11 @@ app.get("/urls", (req, res) => {
 // shows page to create new url
 app.get("/urls/new", isNotLoggedIn, (req, res) => {
 
+  const userId = req.session.user_id;
+  const user = users[userId];
+
   const templateVars = {
-    user: req.cookies["user_id"], // passes user_id to front end conditional
+    user: user, // passes user_id to front end conditional
   };
 
   res.render("urls_new", templateVars);
@@ -39,15 +49,31 @@ app.post("/urls", postURLProtect, (req, res) => {
   const id = generateRandomString(); // generates the rand id key
 
   const { longURL } = req.body;// assigns the longURL value
-  urlDatabase[id] = longURL; // adds to db by key-value pair.
+
+  const userID = req.session.user_id; // Assuming you're storing loggedIn userId in session
+
+
+  // Check if user is logged in
+  if (!userID) {
+    return res.status(403).send("You must be logged in to shorten URLs.");
+  }
+
+  urlDatabase[id] = {
+    longURL: longURL,
+    userID: userID
+  };
 
   res.redirect(`/urls/${id}`); // redirects user to newly created short and long url
 });
 
 // show the register page
 app.get('/register', isLoggedIn, function(req, res, next) {
+  
+  const userId = req.session.user_id;
+  const user = users[userId];
+
   const templateVars = {
-    user: req.cookies["user_id"], // passes user_id to front end conditional
+    user: user, // passes user_id to front end conditional
   };
 
   res.render('register', templateVars);
@@ -76,7 +102,7 @@ app.post("/register", (req, res) => {
   };
 
 
-  res.cookie('user_id', users[id]);
+  req.session.user_id = id;
   res.redirect('/urls');
 
 
@@ -85,8 +111,12 @@ app.post("/register", (req, res) => {
 
 app.get("/login", isLoggedIn, (req, res) => {
 
+
+  const userId = req.session.user_id;
+  const user = users[userId];
+
   const templateVars = {
-    user: req.cookies["user_id"]
+    user: user
   };
 
   res.render('login', templateVars);
@@ -108,14 +138,14 @@ app.post("/login", (req, res) => {
     return res.status(403).send(`Incorrect password. Please try again.`);
   }
 
-  res.cookie("user_id", user);
+  req.session.user_id = user.id;
   res.redirect("/urls");
 
 });
 
 app.post("/logout", (req, res) => {
 
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect("/login");
 
 });
@@ -135,7 +165,7 @@ app.post("/urls/:id/update", (req, res) => {
   const { newLongURL } = req.body;
   const id = req.params.id;
 
-  urlDatabase[id] = newLongURL; // based on the id, updates its longURL
+  urlDatabase[id].longURL = newLongURL; // based on the id, updates its longURL
 
   res.redirect("/urls");
 });
@@ -144,20 +174,24 @@ app.post("/urls/:id/update", (req, res) => {
 app.get("/u/:id", checkShortURL, (req, res) => {
 
   const id = req.params.id;
-  const longURL = urlDatabase[id];
+  const longURL = urlDatabase[id].longURL;
 
   res.redirect(longURL);
 });
 
 // user can search for specific tinyURL code to see its true URL and go to site, if known
 app.get("/urls/:id", (req, res) => {
+
+  const userId = req.session.user_id;
+  const user = users[userId];
+
   // takes the searched parameter in url and shows user the url they were looking for
   const urlID = req.params.id;
   // needs square bracket notation in order to show longURL
   const templateVars = {
     id: urlID,
-    longURL: urlDatabase[urlID],
-    user: req.cookies["user_id"], // passes user_id to front end conditional
+    longURL: urlDatabase[urlID].longURL,
+    user: user, // passes user_id to front end conditional
   };
   res.render(`urls_show`, templateVars);
 });
